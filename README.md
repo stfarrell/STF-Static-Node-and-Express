94ee548475155f6a763e281abcd158576f1723ed
stfarrell <stfarrell93@gmail.com>
2022-06-15 23:56:18 -0600

    Exceeding?

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Style Changes:
 
-1.
+1. Added a picture of a sad ice cream upon receiving a 404 or other error.
+2. Changed the sidebar background color to my favorite shade of green.
+3. Changed the font color and size to better match the new background color.
--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+//Initialize all the things
 const express = require('express');
 const app = express();
 const data = require('./data.json');
 const bodyParser = require('body-parser');
 app.set('view engine', 'pug');
 
+//serve the static files!
 app.use('/public', express.static('public'));
 
+//render the landing page for this portfolio
 app.get('/', (req, res) => {
 	res.locals = data.projects;
-	console.log(res.locals);
 	res.render('index', { projects: data.projects });
 });
 
+//render the about page for this portfolio + test a 500 error (commented out)
 app.get('/about', (req, res, next) => {
 	// const testError = new Error('uh oh');
 	// testError.status = 500;
@@ -19,8 +22,8 @@ app.get('/about', (req, res, next) => {
 	res.render('about');
 });
 
+//Set variable routes using :id
 app.get('/project/:id', (req, res, next) => {
-	console.dir(req.params);
 	if (req.params.id < data.projects.length) {
 		res.render('project', {
 			projectName: data.projects[req.params.id].project_name,
@@ -33,18 +36,20 @@ app.get('/project/:id', (req, res, next) => {
 	next();
 });
 
+//Handle 404 errors
 app.use((req, res, next) => {
 	const err = new Error('Not Found');
 	err.status = 404;
-	console.log(err.message, err.status);
 	res.render('page-not-found', { err });
 });
 
+//Handle other errors
 app.use((err, req, res, next) => {
 	res.status(500);
 	res.render('error', { err });
 });
 
+//Listen for this application on port 3000
 app.listen(3000, () => {
 	console.log('I think its working!');
 });
